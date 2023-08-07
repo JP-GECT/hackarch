ad7aa57da3c3038128d4a79746cb43d088134c15
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2023-08-08 00:15:36 +0530

    created head page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import Image from 'next/image'
+import Example from './components/headpage'
 
 export default function Home() {
   return (
   <>
-  <div className="flex items-center justify-center min-h-screen">
-      <div className="bg-gray-100 p-6 rounded-lg shadow-md max-w-md w-full">
-    
-      </div>
-    </div></>
+  <Example />
+  
+  
+    </>
   )
 }
