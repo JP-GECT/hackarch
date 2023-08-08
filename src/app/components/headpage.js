3e616af7e38a8c5af8968303a25f8a192f33aae9
Vishakh Prakash <vishakhpro2002@gmail.com>
2023-08-08 19:11:58 +0530

    add navbar

--- src/app/components/headpage.js
+++ src/app/components/headpage.js
@@ -4,10 +4,10 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import TypingImage from '../components/TypingImage';
 import TypingText from '../components/TypingText';
-import LogoCard from '../components/card';
-import { PathSvg } from '../components/pathsvg';
+import LogoCard from './card';
+import { PathSvg } from './pathsvg';
 import { motion, useScroll } from "framer-motion"
-import Nav from '../components/navbar';
+
 
 const navigation = [
   { name: 'Product', href: '#' },
@@ -17,14 +17,62 @@ const navigation = [
 ]
 
 export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  
   const { scrollYProgress } = useScroll();
 
   return (
     <>
     <motion.div style={{ scaleX: scrollYProgress }} className='fixed inset-x-0 top-0 bg-green-500 h-2 origin-left ' />  
     <div className="bg-white">
-    <Nav/>
+      <header className="absolute inset-x-0 top-0 z-50">
+       
+        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+          <div className="fixed inset-0 z-50" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
+                />
+              </a>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
 
       <div className="relative isolate px-6 pt-0 lg:px-8">
         <div
